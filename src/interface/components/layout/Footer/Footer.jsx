b5bd95110f02ed71e1d43a722cdad8c31258106e
pamyszz <pamelamelllo560@gmail.com>
2024-06-10 21:53:37 -0300

    footer p1

--- src/interface/components/layout/Footer/Footer.jsx
+++ src/interface/components/layout/Footer/Footer.jsx
@@ -1,23 +1,27 @@
+import React from 'react';
 import footerBg from '../../../../assets/images/imgs-footer/footer-bg.png';
+import logo from '../../../../assets/images/imgs-footer/logo-footer.png';
 import visaIcon from '../../../../assets/images/imgs-footer/Visa.png';
 import mastercardIcon from '../../../../assets/images/imgs-footer/MasterCard.png';
 import amexIcon from '../../../../assets/images/imgs-footer/amex.png';
 import dinersIcon from '../../../../assets/images/imgs-footer/discovery.png';
+import barcodeIcon from '../../../../assets/images/imgs-footer/boleto.png';
+import pixIcon from '../../../../assets/images/imgs-footer/pix.png';
 import instagramIcon from '../../../../assets/images/imgs-footer/Instagram.png';
 import tiktokIcon from '../../../../assets/images/imgs-footer/TikTok.png';
-import GithubIcon from '../../../../assets/images/imgs-footer/GitHub.png';
-import LinktreeIcon from '../../../../assets/images/imgs-footer/Linktree.png';
-import LinkedinIcon from '../../../../assets/images/imgs-footer/LinkedIn.png';
+import githubIcon from '../../../../assets/images/imgs-footer/GitHub.png';
+import linktreeIcon from '../../../../assets/images/imgs-footer/Linktree.png';
+import linkedinIcon from '../../../../assets/images/imgs-footer/LinkedIn.png';
 import './Footer.css';
 import '../../../../../src/App.css';
 
 const Footer = () => {
     return (
-        <div className='footer'>
-            <img src={footerBg} alt="Footer Background" className='footer-background' />
-            <div className='footer-content'>
-                <div className='footer-column'>
-                    <h4>Atendimento ao Cliente</h4>
+        <footer className="footer" style={{ backgroundImage: `url(${footerBg})` }}>
+            <div className="footer-content">
+                <div className="footer-column">
+                    <img src={logo} alt="Logo" className="footer-logo" />
+                    <h4>ATENDIMENTO AO CLIENTE</h4>
                     <ul>
                         <li><a href="#">Venda na Modelly</a></li>
                         <li><a href="#">Fale Conosco</a></li>
@@ -25,38 +29,42 @@ const Footer = () => {
                         <li><a href="#">Preferências e Cookies</a></li>
                     </ul>
                 </div>
-                <div className='footer-column'>
-                    <h4>Sobre a Modelly</h4>
+                <div className="footer-column">
+                    <h4>SOBRE A MODELLY</h4>
                     <ul>
                         <li><a href="#">Sobre Nós</a></li>
                         <li><a href="#">Políticas Modelly</a></li>
                         <li><a href="#">Políticas de Privacidade</a></li>
-                        <li><a href="#">Parceiros</a></li>
+                        <li><a href="#">Parcerias</a></li>
                     </ul>
                 </div>
-                <div className='footer-column'>
-                    <h4>Métodos de Pagamento</h4>
-                    <div className='payment-icons'>
+                <div className="footer-column">
+                    <h4>MÉTODOS DE PAGAMENTO</h4>
+                    <div className="payment-icons">
                         <img src={visaIcon} alt="Visa" />
                         <img src={mastercardIcon} alt="Mastercard" />
                         <img src={amexIcon} alt="American Express" />
-                        <img src={dinersIcon} alt="Diners Club" />
+                        <img src={dinersIcon} alt="Discover" />
+                        <img src={barcodeIcon} alt="Barcode" />
+                        <img src={pixIcon} alt="Pix" />
                     </div>
                 </div>
-                <div className='footer-column'>
-                    <h4>Siga-nos</h4>
-                    <div className='social-icons'>
-                        <img src={instagramIcon} alt="Instagram" />
-                        <img src={tiktokIcon} alt="TikTok" />
-                        <img src={GithubIcon} alt="Github" />
-                        <img src={LinktreeIcon} alt="Linktree" />
-                        <img src={LinkedinIcon} alt="Linkedin" />
+                <div className="footer-column">
+                    <h4>SIGA-NOS</h4>
+                    <div className="social-icons">
+                        <a href="https://www.instagram.com"><img src={instagramIcon} alt="Instagram" /></a>
+                        <a href="https://www.tiktok.com"><img src={tiktokIcon} alt="TikTok" /></a>
+                        <a href="https://www.linkedin.com"><img src={linkedinIcon} alt="LinkedIn" /></a>
+                        <a href="https://www.github.com"><img src={githubIcon} alt="GitHub" /></a>
+                        <a href="https://www.linktree.com"><img src={linktreeIcon} alt="Linktree" /></a>
                     </div>
                 </div>
             </div>
-        </div>
-    )
-}
+            <div className="footer-bottom">
+                <span>© 2024 Modelly. Todos os direitos reservados.</span>
+            </div>
+        </footer>
+    );
+};
 
 export default Footer;
-
