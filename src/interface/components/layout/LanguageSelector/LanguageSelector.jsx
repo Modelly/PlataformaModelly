80c034d6da3abe62ffbb6fbc7afb478d5ba4a9cf
Matheus R. Morgado <matheusrmorgado@hotmail.com>
2024-06-15 22:49:20 -0300

    botão troca de idioma

--- src/interface/components/layout/LanguageSelector/LanguageSelector.jsx
+++ src/interface/components/layout/LanguageSelector/LanguageSelector.jsx
@@ -1,17 +1,50 @@
+import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import styles from './LanguageSelector.module.css';
 
+import ptFlag from "../../../../assets/images/imgs-flagLanguages/pt.png"
+import enFlag from "../../../../assets/images/imgs-flagLanguages/en.png"
+
 const LanguageSelector = () => {
     const { i18n } = useTranslation();
+    const [showDropdown, setShowDropdown] = useState(false);
 
-    const changeLanguage = (lng) => {
-        i18n.changeLanguage(lng);
+    const changeLanguage = (language) => {
+        i18n.changeLanguage(language);
+        setShowDropdown(false);
     };
 
+    const currentLanguage = i18n.language;
+    const languages = {
+        en: {
+            name: 'English',
+            flag: enFlag
+        },
+        pt: {
+            name: 'Português',
+            flag: ptFlag
+        },
+    }
+
     return (
-        <div className={styles.language_selector}>
-            <button className={styles.language_button} onClick={() => changeLanguage('pt')}>Português</button>
-            <button className={styles.language_button} onClick={() => changeLanguage('en')}>English</button>
+        <div className={styles.languageSelector}>
+            <button onClick={() => setShowDropdown(!showDropdown)} className={styles.fixedButton}>
+                <img src={languages[currentLanguage].flag} alt={`${languages[currentLanguage].name}language flag`} />
+                {languages[currentLanguage].name}
+            </button>
+
+            {
+                showDropdown && (
+                    <div className={styles.dropdown}>
+                        {Object.keys(languages).map((lang) => (
+                            <button key={lang} onClick={() => changeLanguage(lang)} className={styles.dropdownButton}>
+                            <img src={languages[lang].flag} alt={languages[lang].name} className={styles.flag} />
+                            {languages[lang].name}
+                            </button>
+                        ))}
+                    </div>
+                )
+            } 
         </div>
     );
 };
