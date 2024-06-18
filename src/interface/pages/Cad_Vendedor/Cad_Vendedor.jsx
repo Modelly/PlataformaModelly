042b1376f1bb3215f38fe8a7d10ed3c61af4cb88
pamyszz <pamelamelllo560@gmail.com>
2024-06-18 20:52:28 -0300

    cad_vendedor criado em pages

---
+++ src/interface/pages/Cad_Vendedor/Cad_Vendedor.jsx
@@ -0,0 +1,15 @@
+import CadastroVendedor from "../Cad_Vendedor";
+import Navbar from "../../components/layout/Navbar/Navbar";
+import Footer from "../../components/layout/Footer/Footer";
+
+function CadastroVendedor() {
+    return (
+        <div>
+            <Navbar />
+            <CarrinhoLayout />
+            <Footer />
+        </div>
+    )
+}
+
+export default CadastroVendedor;
