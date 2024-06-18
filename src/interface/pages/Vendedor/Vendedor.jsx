416e65e1df1bed8ac7124b0bfd14555443a9c0fe
pamyszz <pamelamelllo560@gmail.com>
2024-06-18 20:41:47 -0300

    seja vendedor

--- src/interface/pages/Vendedor/Vendedor.jsx
+++ src/interface/pages/Vendedor/Vendedor.jsx
@@ -1,9 +1,10 @@
-import VendedorLayout from '../../components/layout/Seja_Vendedor/VendedorLayout.jsx'
+import VendedorLayout from '../../components/layout/Seja_Vendedor/VendedorLayout.jsx';
 import Navbar from '../../components/layout/Navbar/Navbar.jsx';
+import React from 'react';
 
 const Vendedor = () => {
   return (
-    <div>
+    <div className="bodyHTML">
       <Navbar />
       <VendedorLayout />
     </div>
