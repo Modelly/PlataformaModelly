1287c19361ab49bc2aff3d3c6cd00e833adfc26d
Matheus R. Morgado <matheusrmorgado@hotmail.com>
2024-06-25 10:54:13 -0300

    Dropdown NavConsumidor

--- src/interface/components/layout/Navbar/consumidor/NavbarConsumidor.jsx
+++ src/interface/components/layout/Navbar/consumidor/NavbarConsumidor.jsx
@@ -1,12 +1,29 @@
-import { Link } from 'react-router-dom';
-import styleNav from '../Navbar.module.css';
-import logo from '../../../../../assets/images/imgs-nav/modelly-logo.png';
+import { getBasePath } from "../../../util/GetBasePath.jsx"
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+
 import lupa from '../../../../../assets/images/imgs-nav/lupa-icon.png';
-import bagIcon from '../../../../../assets/images/imgs-nav/consumidor/atencao-sacola.png';
+import logo from '../../../../../assets/images/imgs-nav/modelly-logo.png';
 import heartIcon from '../../../../../assets/images/imgs-nav/heart-icon.png';
 import photoPerfil from '../../../../../assets/images/imgs-nav/foto-perfil.png';
+import bagIcon from '../../../../../assets/images/imgs-nav/consumidor/atencao-sacola.png';
+
+import helpIcon from "../../../../../assets/images/imgs-nav/dropdown/Help.png"
+import storeIcon from "../../../../../assets/images/imgs-nav/dropdown/Store.png"
+import logoutIcon from "../../../../../assets/images/imgs-nav/dropdown/Logout.png"
+import settingsIcon from "../../../../../assets/images/imgs-nav/dropdown/Settings.png"
+
+import styleNav from '../Navbar.module.css';
 
 const NavbarConsumidor = () => {
+    const navigate = useNavigate()
+    const location = useLocation();
+    const basePath = getBasePath(location.pathname);
+
+    const handleLogout = () => {
+        /*Lógica de logout vai vir aqui quando eu terminar o back*/
+        navigate(`/`);
+    };
+
     return (
         <div className={styleNav.navbar_container}>
             <div className={styleNav.navbar_top}>
@@ -25,7 +42,27 @@ const NavbarConsumidor = () => {
                         <img src={bagIcon} alt="Carrinho" className={styleNav.iconConsumidor} />
                     </Link>
                     <div>
-                        <img src={photoPerfil} alt="Ícone de usuário" className={styleNav.user_iconPerfil} />
+                        <details className={styleNav.dropdown}>
+                            <summary><img src={photoPerfil} alt="Ícone de usuário" className={styleNav.user_iconPerfil} /></summary>
+                            <ul className={styleNav.dropdown_menu}>
+                                <li>
+                                    <img src={settingsIcon} alt="icone de engranagem" />
+                                    Configurar Conta
+                                </li>
+                                <li onClick={() => navigate(`${basePath}/seja-vendedor`)}>
+                                    <img src={storeIcon} alt="icone de engranagem" />
+                                    Seja Vendedor
+                                </li>
+                                <li onClick={() => navigate(`${basePath}/fale-conosco`)}>
+                                    <img src={helpIcon} alt="icone de engranagem" />
+                                    Ajuda
+                                </li>
+                                <li onClick={handleLogout}>
+                                    <img src={logoutIcon} alt="icone de engranagem" />
+                                    Encerrar sessão
+                                </li>
+                            </ul>
+                        </details>
                     </div>
                 </div>
             </div>
