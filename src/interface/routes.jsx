98810c36f347e1999aa94c51b4bb06e1fdcfd80f
pamyszz <pamelamelllo560@gmail.com>
2024-06-29 18:10:56 -0300

    esqueleto compra checkout

--- src/interface/routes.jsx
+++ src/interface/routes.jsx
@@ -1,15 +1,13 @@
+// AppRoutes.jsx
+import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import ScrollToTop from './components/util/ScrollToTop.jsx';
 
-// paginas por tipo de usuario
 import UsuarioLogado from './pages/UsuarioLogado.jsx';
 import UsuarioDeslogado from './pages/UsuarioDeslogado.jsx';
 import UsuarioArtesao from './pages/UsuarioArtesao.jsx';
 
-
 import Login from '../interface/components/layout/login/LoginLayout.jsx';
-
-// layouts das paginas
 import HomeLayout from '../interface/components/layout/home/HomeLayout.jsx';
 import Produto from '../interface/components/layout/Produto/ProdutoLayout.jsx';
 import FeiraVirtual from '../interface/components/layout/FeiraVirtual/FeiraLayout.jsx';
@@ -25,7 +23,7 @@ import CategoriaCliente from './components/layout/Personalizado/pedidoCliente/Ca
 import FormularioServicos from './components/layout/Personalizado/pedidoCliente/FormularioServicos.jsx';
 import EscolhaCategoria from './components/layout/Personalizado/pedidoCliente/EscolhaCategoria.jsx';
 import Publicar from './components/layout/Personalizado/pedidoCliente/Publicar.jsx';
-
+import CheckoutLayout from './components/layout/Carrinho/Checkout/CheckoutLayout.jsx';
 
 const AppRoutes = () => {
     return (
@@ -33,61 +31,63 @@ const AppRoutes = () => {
             <ScrollToTop />
             <div className='container'>
                 <Routes>
+                    <Route path="/login" element={<Login />} />
                     
-                    <Route path="/login" element={<Login/>} />
-                    
-                    {/* TELAS CONSUMIDOR NÃO LOGADO */}
-                    <Route path="/" element={<UsuarioDeslogado/>}>
-                        <Route path="/" element={<HomeLayout/>}/>
-                        <Route path="/sobre" element={<Sobre/>}/>
-                        <Route path="/produto" element={<Produto/>}/>
-                        <Route path="/carrinho" element={<Carrinho/>}/>
-                        <Route path="/categoria" element={<Categoria/>}/>
-                        <Route path="/produto/:id" element={<Produto/>}/>
-                        <Route path="/fale-conosco" element={<Contato_1/>}/>
-                        <Route path="/feira-virtual" element={<FeiraVirtual/>}/>
-                        <Route path="/seja-vendedor" element={<VendedorPopUp/>}/>
-                        <Route path="/cadastrar-vendedor" element={<CadastroVendedor/>}/>
+                    {/* Rotas para usuários não logados */}
+                    <Route path="/" element={<UsuarioDeslogado />}>
+                        <Route index element={<HomeLayout />} />
+                        <Route path="/sobre" element={<Sobre />} />
+                        <Route path="/produto" element={<Produto />} />
+                        <Route path="/carrinho" element={<Carrinho />} />
+                        <Route path="/categoria" element={<Categoria />} />
+                        <Route path="/produto/:id" element={<Produto />} />
+                        <Route path="/fale-conosco" element={<Contato_1 />} />
+                        <Route path="/feira-virtual" element={<FeiraVirtual />} />
+                        <Route path="/seja-vendedor" element={<VendedorPopUp />} />
+                        <Route path="/cadastrar-vendedor" element={<CadastroVendedor />} />
                     </Route>
 
-                    {/* TELAS CONSUMIDOR LOGADO */}
-                    <Route path="/consumidor" element={<UsuarioLogado/>}>
-                        <Route path="/consumidor" element={<HomeLayout/>}/>
-                        <Route path="/consumidor/sobre" element={<Sobre/>}/>
-                        <Route path="/consumidor/produto" element={<Produto/>}/>
-                        <Route path="/consumidor/carrinho" element={<Carrinho/>}/>
-                        <Route path="/consumidor/categoria" element={<Categoria/>}/>
-                        <Route path="/consumidor/produto/:id" element={<Produto/>}/>
-                        <Route path="/consumidor/fale-conosco" element={<Contato_1/>}/>
-                        <Route path="/consumidor/feira-virtual" element={<FeiraVirtual/>}/>
-                        <Route path="/consumidor/seja-vendedor" element={<VendedorPopUp/>}/>
-                        <Route path="/consumidor/cadastrar-vendedor" element={<CadastroVendedor/>}/>
-                        <Route path="/consumidor/pedido-personalizado" element={<CategoriaCliente/>}/>
+                    {/* Rotas para usuários logados */}
+                    <Route path="/consumidor" element={<UsuarioLogado />}>
+                        <Route index element={<HomeLayout />} />
+                        <Route path="/consumidor/sobre" element={<Sobre />} />
+                        <Route path="/consumidor/produto" element={<Produto />} />
+                        <Route path="/consumidor/carrinho" element={<Carrinho />} />
+                        <Route path="/consumidor/categoria" element={<Categoria />} />
+                        <Route path="/consumidor/produto/:id" element={<Produto />} />
+                        <Route path="/consumidor/fale-conosco" element={<Contato_1 />} />
+                        <Route path="/consumidor/feira-virtual" element={<FeiraVirtual />} />
+                        <Route path="/consumidor/seja-vendedor" element={<VendedorPopUp />} />
+                        <Route path="/consumidor/cadastrar-vendedor" element={<CadastroVendedor />} />
+                        <Route path="/consumidor/pedido-personalizado" element={<CategoriaCliente />} />
                     </Route>
 
-                    {/* TELAS ARTESAO */}
+                    {/* Rotas para artesãos */}
                     <Route path="/artesao" element={<UsuarioArtesao />}>
-                        <Route path="/artesao" element={<HomeLayout/>}/>
-                        <Route path="/artesao/sobre" element={<Sobre/>}/>
-                        <Route path="/artesao/produto" element={<Produto/>}/>
-                        <Route path="/artesao/carrinho" element={<Carrinho/>}/>
-                        <Route path="/artesao/categoria" element={<Categoria/>}/>
-                        <Route path="/artesao/produto/:id" element={<Produto/>}/>
-                        <Route path="/artesao/fale-conosco" element={<Contato_1/>}/>
-                        <Route path="/artesao/feira-virtual" element={<FeiraVirtual/>}/>
-                        <Route path="/artesao/seja-vendedor" element={<VendedorPopUp/>}/>
-                        <Route path="/artesao/Cadastro_Produto" element={<CadastroProduto/>}/>
-                        <Route path="/artesao/Personalizado" element={<PersonalizadoLayout/>}/>
-                        <Route path="/artesao/cadastrar-vendedor" element={<CadastroVendedor/>} />
-                        <Route path="/artesao/pedido-personalizado" element={<CategoriaCliente/>}/>
+                        <Route index element={<HomeLayout />} />
+                        <Route path="/artesao/sobre" element={<Sobre />} />
+                        <Route path="/artesao/produto" element={<Produto />} />
+                        <Route path="/artesao/carrinho" element={<Carrinho />} />
+                        <Route path="/artesao/categoria" element={<Categoria />} />
+                        <Route path="/artesao/produto/:id" element={<Produto />} />
+                        <Route path="/artesao/fale-conosco" element={<Contato_1 />} />
+                        <Route path="/artesao/feira-virtual" element={<FeiraVirtual />} />
+                        <Route path="/artesao/seja-vendedor" element={<VendedorPopUp />} />
+                        <Route path="/artesao/Cadastro_Produto" element={<CadastroProduto />} />
+                        <Route path="/artesao/Personalizado" element={<PersonalizadoLayout />} />
+                        <Route path="/artesao/cadastrar-vendedor" element={<CadastroVendedor />} />
+                        <Route path="/artesao/pedido-personalizado" element={<CategoriaCliente />} />
                     </Route>
 
-                    <Route path="/Cadastro_Produto" element={<CadastroProduto/>} />
+                    {/* Rota para checkout */}
+                    <Route path="/checkout" element={<CheckoutLayout />} />
 
-                    <Route path="/pedido-personalizado" element={<CategoriaCliente/>}/>
-                    <Route path="/pedido-personalizado/categorias" element={<EscolhaCategoria/>}/>
-                    <Route path="/pedido-personalizado/formulario" element={<FormularioServicos/>}/>
-                    <Route path="/pedido-personalizado/publicar" element={<Publicar/>}/>
+                    {/* Rotas para pedido personalizado */}
+                    <Route path="/pedido-personalizado" element={<CategoriaCliente />} />
+                    <Route path="/pedido-personalizado/categorias" element={<EscolhaCategoria />} />
+                    <Route path="/pedido-personalizado/formulario" element={<FormularioServicos />} />
+                    <Route path="/pedido-personalizado/publicar" element={<Publicar />} />
+                  
                 </Routes>         
             </div>
         </Router>
